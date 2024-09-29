ec969214837ce20686db955087cbf7946ae7533e
Zeyad Mohamed <zeyadmohamedali@gmail.com>
2024-09-29 17:41:14 +0300

    FirstCommit

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+[ApiController, Route("api/[controller]")]
+public class ProductController: ControllerBase
+{
+    private readonly StoreContext _context;
+    public ProductController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var products =await _context.Products.ToListAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if(product == null)
+            return NotFound();
+        return Ok(product);
+    }
+    
+    
+}
